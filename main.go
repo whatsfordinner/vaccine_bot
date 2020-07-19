142541a8163ab697056bb2c82cd6df1dc09c4b83
what's for dinner <cruelangeleo7@gmail.com>
2020-07-19 17:01:07 +1000

    disease list and selection plus tests

--- main.go
+++ main.go
@@ -2,11 +2,69 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type diseaseList struct {
+	Diseases []string `json:"diseases"`
+}
+
+func getDiseaseListFromFile(diseaseFilename string) (*diseaseList, error) {
+	diseases := new(diseaseList)
+	diseaseFile, err := os.Open(diseaseFilename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer diseaseFile.Close()
+	byteValue, err := ioutil.ReadAll(diseaseFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(byteValue), diseases)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(diseases.Diseases) == 0 {
+		errorString := fmt.Sprintf("Source JSON %s is malformed or has no diseases", diseaseFilename)
+		return nil, errors.New(errorString)
+	}
+
+	return diseases, nil
+}
+
+func (dl *diseaseList) getDisease() (string, error) {
+	rand.Seed(time.Now().UnixNano())
+
+	if len(dl.Diseases) == 0 {
+		return "", errors.New("List of diseases has no entries")
+	}
+
+	return dl.Diseases[rand.Intn(len(dl.Diseases))], nil
+}
+
+func buildTweet(disease string) string {
+	return ""
+}
+
+func sendTweet() {
+
+}
 
 func handleRequest(ctx context.Context, req events.CloudWatchEvent) {
 	log.Printf("Handling new request")
--- main_test.go
+++ main_test.go
@@ -1,1 +1,81 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetDiseaseListFromFile(t *testing.T) {
+	tests := map[string]struct {
+		filename  string
+		shouldErr bool
+	}{
+		"valid filename with valid JSON":   {"test-fixtures/diseases.json", false},
+		"valid filename with invalid JSON": {"test-fixtures/malformed.json", true},
+		"invalid filename":                 {"test-fixtures/foobarbaz", true},
+	}
+
+	testDiseases := new(diseaseList)
+	testDiseases.Diseases = []string{"foo", "bar"}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result, err := getDiseaseListFromFile(test.filename)
+			if test.shouldErr && err == nil {
+				t.Fatalf("Expected error, but no error occured")
+			}
+
+			if !test.shouldErr && err != nil {
+				t.Fatalf("Expected no error, but got: %s", err.Error())
+			}
+
+			if !reflect.DeepEqual(result, testDiseases) && !test.shouldErr {
+				t.Fatalf("Results did not match\nGot: %+v\nExpected: %+v", result, testDiseases)
+			}
+		})
+	}
+}
+
+func TestGetDisease(t *testing.T) {
+	testDiseases := new(diseaseList)
+	testDiseases.Diseases = []string{"foo", "bar", "baz"}
+
+	testMalformed := new(diseaseList)
+	testMalformed.Diseases = []string{}
+
+	tests := map[string]struct {
+		input     *diseaseList
+		expected  []string
+		shouldErr bool
+	}{
+		"valid diseaseList":   {testDiseases, []string{"foo", "bar", "baz"}, false},
+		"invalid diseaseList": {testMalformed, []string{}, true},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result, err := test.input.getDisease()
+			if test.shouldErr && err == nil {
+				t.Fatalf("Expected error, but no error occurred")
+			}
+
+			if !test.shouldErr && err != nil {
+				t.Fatalf("Expected no error, but got: %s", err.Error())
+			}
+
+			if !test.shouldErr && !stringInSlice(result, test.expected) {
+				t.Fatalf("Result not in expect list\nGot: %s\nExpected one from: %v", result, test.expected)
+			}
+		})
+	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if a == b {
+			return true
+		}
+	}
+
+	return false
+}
